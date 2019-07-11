76642a004fcdf93b44ee5a838c675f8eadcdecb7
Ernesto <hooghkirk.e@gmail.com>
2019-07-11 00:55:31 -0700

    4th commit

--- contentscript.js
+++ contentscript.js
@@ -42,65 +42,106 @@ function checkExists()
 		document.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(popcornImage); 
 
 		//First i need to get the name of the item
-		var x = document.getElementsByClassName("av-hover-content av-narrow")[0].firstChild.innerHTML;
-		var z = x.indexOf('>');
-		var titleOfMedia = "";
-		z += 1;
+		// var x = document.getElementsByClassName("av-hover-content av-narrow")[0].firstChild.innerHTML;
+		// var z = x.indexOf('>');
+		// var titleOfMedia = "";
+		// z += 1;
 
-		while (x.charAt(z) != '<')
-		{
-			if(x.charAt(z) == ' ')
-			{
-			titleOfMedia = titleOfMedia + '_';
-			}
-			else if((x.charAt(z) == ',') || (x.charAt(z) == '!') || (x.charAt(z) == ':') || (x.charAt(z) == "'" ))
-			{}
-			else
-			{
-			titleOfMedia = titleOfMedia + x.charAt(z);
-			}
-			++z;
-		}
+		// while (x.charAt(z) != '<')
+		// {
+		// 	if(x.charAt(z) == ' ')
+		// 	{
+		// 	titleOfMedia = titleOfMedia + '_';
+		// 	}
+		// 	else if((x.charAt(z) == ',') || (x.charAt(z) == '!') || (x.charAt(z) == ':') || (x.charAt(z) == "'" ))
+		// 	{}
+		// 	else
+		// 	{
+		// 	titleOfMedia = titleOfMedia + x.charAt(z);
+		// 	}
+		// 	++z;
+		// }
 
-		console.log(titleOfMedia);
-		console.log(z);
-		console.log(x);
+		// console.log(titleOfMedia);
+		// console.log(z);
+		// console.log(x);
 
-		//Then i need to pass that in to a text variable
-		//Then i need to send that text variable to rotten tomatoes
-		//to their search
-		//grab first link
-		//get to first link page
+		// const url = 'https://rottentomatoes.com/m/' + titleOfMedia;
 
-		// Url's passed in
-		const url = 'https://rottentomatoes.com/m/' + titleOfMedia;
 		const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-		fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+		// console.log(proxyurl + url);
+
+		var titleOfMedia = document.getElementsByClassName("av-hover-content av-narrow")[0].firstChild.firstChild.innerText;
+		
+		var formattedTitle = titleOfMedia.split(' ').join('%20');
+
+		console.log(formattedTitle);
+
+		url = proxyurl + 'https://google.com/search?q=rotten%20tomatoes%20' + formattedTitle + '%20rating';
+
+		console.log(url);
+
+		fetch(url) // https://cors-anywhere.herokuapp.com/https://example.com
     		.then(function(response) {
     		return response.text()
     		})
     	.then(function(html) {
 			var parser = new DOMParser();
 			var doc = parser.parseFromString(html, "text/html");
-			//console.log(doc);
-			var docArticle = doc.getElementsByClassName("mop-ratings-wrap__percentage")[1].innerHTML;
-			console.log(docArticle);
+			var rtLink = doc.getElementsByClassName("iUh30")[0].innerText;
 
-			// Creates a new span, ...
-			var ratingSpan = document.createElement("span");
-			var textnode = document.createElement("p");
-			textnode.innerHTML = docArticle;
-			ratingSpan.appendChild(textnode);
+			console.log(rtLink);
 
-			// 
-			RTspan.style.display = "flex";
+			var res = rtLink.split("www.");
+			console.log(res[1]);
 
-			// 
-			ratingSpan.setAttribute("style","padding-top:8px; padding-left:4px;");
+			url2 = proxyurl + 'https://' + res[1];
+		
+			console.log(url2);
 
-			//... and appends it to the RTspan
-			document.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(ratingSpan); 
+			fetch(url2)
+				.then(function(response) {
+				return response.text()
+				})
+			.then(function(html) {
+				var parser = new DOMParser();
+				var doc = parser.parseFromString(html, "text/html");
+				var docArticle;
+
+				if(doc.getElementsByClassName("mop-ratings-wrap__prerelease-text")[0])
+				{
+					if(doc.getElementsByClassName("mop-ratings-wrap__percentage")[0].innerHTML)
+					{
+					docArticle = doc.getElementsByClassName("mop-ratings-wrap__percentage")[0].innerHTML;
+					}
+					else
+					{
+					docArticle = "DNE";
+					}
+				}
+				else
+				{
+					docArticle = doc.getElementsByClassName("mop-ratings-wrap__percentage")[1].innerHTML;
+				}
+				console.log(docArticle);	
+				
+				// Creates a new span, ...
+				var ratingSpan = document.createElement("span");
+				var textnode = document.createElement("p");
+				textnode.innerHTML = docArticle;
+				ratingSpan.appendChild(textnode);
+
+				// 
+				RTspan.style.display = "flex";
+
+				// 
+				ratingSpan.setAttribute("style","padding-top:8px; padding-left:4px;");
+
+				//... and appends it to the RTspan
+				document.getElementsByClassName("Rotten-Tomatoes")[0].appendChild(ratingSpan); 
+			})
+			
 
 			})
 			.catch(function(err) {  
